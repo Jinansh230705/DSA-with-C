69b93d5d5c0edf9a852f993525cac385240a7127
Jinansh <149217594+Jinansh230705@users.noreply.github.com>
2024-06-28 18:20:38 +0530

    Create Stack Operations.c

--- Stack Operations.c
+++ Stack Operations.c
@@ -1,76 +1,94 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #define MAX 5
+  int top=-1;
+  int stack[MAX];
 
-int top = -1;
-int* stack;
+  	void push()
+	{
+		if(top>=MAX-1)
+		{
+		printf("\nStack Overflow");
+    	}
+		else
+		{
+		printf("Enter the element to Push:");
+		top++;
+		scanf("%d",&stack[top]);	
+		}
+	}
+	
+	void pop()
+	{
+		if(top<0)
+		{
+		printf("\nStack Underflow");
+    	}
+		else
+		{
+		top--;
+		}
+	}
+	
+	void peak()
+	{
+		if(top<0)
+		{
+			printf("\nStack is Empty");
+		}
+		else
+		{
+			printf("%d",stack[top]);
+		}
+	}
+	
+	void display()
+	{
+		if(top<0)
+		{
+			printf("\nStack is Empty");
+		}
+		else
+		{   
+		    int i;
+		    for(i=top;i>=0;i--)
+		    {
+			printf("\n%d",stack[i]);
+		    }
+		}
+	}
+	
+  int main()
+    {
+      int choice;
+	   do
+	   {
+	   	printf("\n------------------------");
+	   	printf("\n1.PUSH\n2.POP\n3.PEAK\n4.DISPLAY\n5.EXIT");
+	   	printf("\nEnter your Choice:");
+	   	scanf("%d",&choice);
+	   	
+	   	switch(choice)
+	   	{
+	   	      case 1:
+			  push();
+			  break;
+			  case 2:
+			  pop();
+			  break;
+			  case 3:
+			  peak();
+			  break;
+			  case 4:
+			  display();
+			  break;
+			  case 5:
+			  printf("Exit");
+			  break;
+			  default:
+			  printf("Invilid Input");	
+		}
+	   }while(choice!=5);
+	}  
+	
 
-void push() {
-    if (top >= MAX - 1) {
-        printf("Stack Overflow\n");
-    } else {
-        printf("Enter the number to Push: ");
-        top++;
-        scanf("%d", &stack[top]);
-    }
-}
-
-void pop() {
-    if (top < 0) {
-        printf("Stack Underflow\n");
-    } else {
-        top--;
-    }
-}
-
-void peak() {
-    if (top >= 0) {
-        printf("Top element: %d\n", stack[top]);
-    } else {
-        printf("Stack is empty\n");
-    }
-}
-
-void display() {
-    if (top >= 0) {
-        for (int i = top; i >= 0; i--) {
-            printf("%d\n", stack[i]);
-        }
-    } else {
-        printf("Stack is empty\n");
-    }
-}
-
-int main() {
-    int choice;
-    stack = (int*)calloc(MAX, sizeof(int));
-
-    do {
-        printf("\n---------------------------------------");
-        printf("\nPerform Operations");
-        printf("\n1. PUSH\n2. POP\n3. PEAK\n4. DISPLAY\n5. EXIT\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                push();
-                break;
-            case 2:
-                pop();
-                break;
-            case 3:
-                peak();
-                break;
-            case 4:
-                display();
-                break;
-            case 5:
-                printf("Exited\n");
-                break;
-            default:
-                printf("Invalid Selection\n");
-        }
-    } while (choice != 5);
-    return 0;
-}
