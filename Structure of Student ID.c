313ffea7eeb8d0eff9fb80e2147106741475b87b
Jinansh <149217594+Jinansh230705@users.noreply.github.com>
2024-06-28 19:25:18 +0530

    Create Structure of Student ID.c

--- Structure of Student ID.c
+++ Structure of Student ID.c
@@ -1,1 +1,34 @@
+struct student 
+{
+    int id;
+    char gender;
+    float per;
+};
+
+int main()
+{
+    struct student std[3];
+    struct student *ptr=std;
+    
+ int i;
+ for(i=0;i<3;i++)
+ {
+  printf("ENTER ID\n");
+  scanf("%d",&(ptr+i)->id);
+  printf("ENTER GENDER\n");
+  scanf(" %c",&(ptr+i)->gender);
+  printf("ENTER PER\n");
+  scanf("%f",&(ptr+i)->per);
+ }
+printf("ID\tGENDER\tPER\n");
+for(i=0;i<3;i++)
+ {
+     printf("%d\t%c\t    %.2f",(ptr+i)->id,(ptr+i)->gender,(ptr+i)->per);
+     printf("\n");
+ }
+  
+    return 0;
+}
+
+
 
