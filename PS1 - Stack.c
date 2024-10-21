bec6085630cc0b0bf31c1401c2d7e32eee87c64b
Jinansh <149217594+Jinansh230705@users.noreply.github.com>
2024-10-21 15:30:49 +0530

    Create PS1 - Stack.c

---
+++ PS1 - Stack.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 5
+  int top=-1;
+  int stack[MAX];
+
+  	void push()
+	{
+		if(top>=MAX-1)
+		{
+		printf("\nStack Overflow");
+    	}
+		else
+		{
+		printf("Enter the element to Push:");
+		top++;
+		scanf("%d",&stack[top]);	
+		}
+	}
+	
+	void pop()
+	{
+		if(top<0)
+		{
+		printf("\nStack Underflow");
+    	}
+		else
+		{
+		top--;
+		}
+	}
+	
+	void peak()
+	{
+		if(top<0)
+		{
+			printf("\nStack is Empty");
+		}
+		else
+		{
+			printf("%d",stack[top]);
+		}
+	}
+	
+	void display()
+	{
+		if(top<0)
+		{
+			printf("\nStack is Empty");
+		}
+		else
+		{   
+		    int i;
+		    for(i=top;i>=0;i--)
+		    {
+			printf("\n%d",stack[i]);
+		    }
+		}
+	}
+	
+  int main()
+    {
+      int choice;
+	   do
+	   {
+	   	printf("\n------------------------");
+	   	printf("\n1.PUSH\n2.POP\n3.PEAK\n4.DISPLAY\n5.EXIT");
+	   	printf("\nEnter your Choice:");
+	   	scanf("%d",&choice);
+	   	
+	   	switch(choice)
+	   	{
+	   	      case 1:
+			  push();
+			  break;
+			  case 2:
+			  pop();
+			  break;
+			  case 3:
+			  peak();
+			  break;
+			  case 4:
+			  display();
+			  break;
+			  case 5:
+			  printf("Exit");
+			  break;
+			  default:
+			  printf("Invilid Input");	
+		}
+	   }while(choice!=5);
+	}  
+	
